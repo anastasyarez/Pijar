8d46e9d6d17024dc012263c34ac0f0148821fbbe
Anastasya Rezkianissa <anastasyarez.it2@gmail.com>
2019-07-26 14:37:18 +0700

    adding library

---
+++ app/src/main/java/com/pijarteam/pijar/Activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.pijarteam.pijar.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pijarteam.pijar.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
